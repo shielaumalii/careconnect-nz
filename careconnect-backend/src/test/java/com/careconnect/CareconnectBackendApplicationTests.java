74daef7655a49b6a2b27738b08687c8c2bfdd8bd
Shiela Umali <shielamarie.lleva.umali@gmail.com>
2026-07-29 00:37:44 +1200

    Initial commit

---
+++ careconnect-backend/src/test/java/com/careconnect/CareconnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareconnectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
